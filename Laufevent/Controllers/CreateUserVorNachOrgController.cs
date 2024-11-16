10f81d639d59ca1197f8d589fb06db21d4dcc0b3
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2024-11-16 20:48:29 +0100

    Update CreateUserVorNachOrgController.cs

--- Laufevent/Controllers/CreateUserVorNachOrgController.cs
+++ Laufevent/Controllers/CreateUserVorNachOrgController.cs
@@ -28,14 +28,14 @@ public class CreateUserVorNachOrgController : ControllerBase
 
                 using (var command = new SqlCommand(query, connection))
                 {
-                    command.Parameters.AddWithValue("@Vorname", userInfo.firstname);
-                    command.Parameters.AddWithValue("@Nachname", userInfo.lastname);
-                    command.Parameters.AddWithValue("@Runden", laps);
-                    command.Parameters.AddWithValue("@Bestzeit", fastest_lap);
-                    command.Parameters.AddWithValue("@Educardnr", DBNull.Value);
-                    command.Parameters.AddWithValue("@Klasse", DBNull.Value);
-                    command.Parameters.AddWithValue("@Fruehstarter", DBNull.Value);
-                    command.Parameters.AddWithValue("@Organisation", userInfo.organisation); 
+                    command.Parameters.AddWithValue("@firstname", userInfo.firstname);
+                    command.Parameters.AddWithValue("@lastname", userInfo.lastname);
+                    command.Parameters.AddWithValue("@laps", laps);
+                    command.Parameters.AddWithValue("@fastest_lap", fastest_lap);
+                    command.Parameters.AddWithValue("@educard_number", DBNull.Value);
+                    command.Parameters.AddWithValue("@school_class", DBNull.Value);
+                    command.Parameters.AddWithValue("@early_starter", DBNull.Value);
+                    command.Parameters.AddWithValue("organisation", userInfo.organisation); 
                     var rowsAffected = command.ExecuteNonQuery();
                     return Ok($"Data inserted successfully. Rows affected: {rowsAffected}");
                 }
